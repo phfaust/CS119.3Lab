45b5b76ad056f91f9083fd437252ab4ebf262e28
kurtv13 <kurt.v@outlook.com>
2016-06-09 18:57:25 +0800

    Show toast function fix

--- app/src/main/java/com/kurtbautista/cs1193lab/MainActivity.java
+++ app/src/main/java/com/kurtbautista/cs1193lab/MainActivity.java
@@ -2,6 +2,8 @@ package com.kurtbautista.cs1193lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View v)
+    {
+        EditText username = (EditText)findViewById(R.id.usernameField);
+        EditText password = (EditText)findViewById(R.id.passwordField);
+        CheckBox girememberMe = (CheckBox)findViewById(R.id.rememberMeCheckBox);
+
+        String msg = "Username: " + username.getText().toString() + "\n Password: " + password.getText().toString() + "\n Remember Me?: " + rememberMe.isChecked();
+
+        Toast.makeText(this, msg, Toast.LENGTH_LONG);
     }
 }
